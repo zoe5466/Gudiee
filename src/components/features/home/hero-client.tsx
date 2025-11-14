bce6baaa5b1caa6f69b6e6015b6d1eb36e986542
muyi3738 <muyi3738@gmail.com>
2025-11-14 23:03:53 +0800

    Update hero-client.tsx

--- src/components/features/home/hero-client.tsx
+++ src/components/features/home/hero-client.tsx
@@ -104,7 +104,7 @@ export function HeroClient() {
             color: '#2563eb',
             cursor: 'pointer'
           }}
-          onClick={() => router.push('/')}
+          onClick={() => router.push('/hero-client')}
         >
           Guidee
         </div>
